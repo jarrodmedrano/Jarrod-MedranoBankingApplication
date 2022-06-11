942f8b01a9e3a618678f84b49ff2ee59d8996aeb
Jarrod Medrano <jmedran@gmail.com>
2022-06-11 12:27:06 -0500

    add password validation

--- createaccount.js
+++ createaccount.js
@@ -1,33 +1,42 @@
-function CreateAccount(){
-  const [show, setShow]         = React.useState(true);
-  const [status, setStatus]     = React.useState('');
-  const [name, setName]         = React.useState('');
-  const [email, setEmail]       = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const ctx = React.useContext(UserContext);  
+function CreateAccount() {
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const ctx = React.useContext(UserContext);
 
-  function validate(field, label){
-      if (!field) {
-        setStatus('Error: ' + label);
-        setTimeout(() => setStatus(''),3000);
-        return false;
-      }
-      return true;
+  function validate(field, label) {
+    if (!field) {
+      setStatus("Error: " + label + " is required");
+      setTimeout(() => setStatus(""), 3000);
+      return false;
+    }
+    return true;
   }
 
-  function handleCreate(){
-    console.log(name,email,password);
-    if (!validate(name,     'name'))     return;
-    if (!validate(email,    'email'))    return;
-    if (!validate(password, 'password')) return;
-    ctx.users.push({name,email,password,balance:100});
-    setShow(false);
-  }    
+  function validatePassword(field, label) {
+    if (!/^.{8,50}$/.test(field)) {
+      setStatus("Error: " + label + " must be at least 8 characters");
+      setTimeout(() => setStatus(""), 3000);
+      return false;
+    }
+    return true;
+  }
 
-  function clearForm(){
-    setName('');
-    setEmail('');
-    setPassword('');
+  function handleCreate() {
+    console.log(name, email, password);
+    if (!validate(name, "name")) return;
+    if (!validate(email, "email")) return;
+    if (!validatePassword(password, "password")) return;
+    ctx.users.push({ name, email, password, balance: 100 });
+    setShow(false);
+  }
+
+  function clearForm() {
+    setName("");
+    setEmail("");
+    setPassword("");
     setShow(true);
   }
 
@@ -36,22 +45,59 @@ function CreateAccount(){
       bgcolor="primary"
       header="Create Account"
       status={status}
-      body={show ? (  
-              <>
-              Name<br/>
-              <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
-              Email address<br/>
-              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
-              Password<br/>
-              <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
-              </>
-            ):(
-              <>
-              <h5>Success</h5>
-              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
-              </>
-            )}
+      body={
+        show ? (
+          <>
+            Name
+            <br />
+            <input
+              type="input"
+              className="form-control"
+              id="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.currentTarget.value)}
+            />
+            <br />
+            Email address
+            <br />
+            <input
+              type="input"
+              className="form-control"
+              id="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+            <br />
+            Password
+            <br />
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+            />
+            <br />
+            <button
+              type="submit"
+              className="btn btn-light"
+              onClick={handleCreate}
+            >
+              Create Account
+            </button>
+          </>
+        ) : (
+          <>
+            <h5>Success</h5>
+            <button type="submit" className="btn btn-light" onClick={clearForm}>
+              Add another account
+            </button>
+          </>
+        )
+      }
     />
-  )
+  );
 }
