65a2807e7a45d762af70eba9102463c9dbd36a6e
Jarrod Medrano <jmedran@gmail.com>
2022-06-18 12:38:21 -0500

    add active routes

---
+++ components/nav/navbar.js
@@ -0,0 +1,80 @@
+const useLocation = ReactRouterDOM.useLocation;
+
+const NavBar = (props) => {
+  const location = useLocation();
+  console.log("the location", location);
+
+  const routes = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Create Account",
+      href: "/CreateAccount/",
+    },
+    {
+      name: "Login",
+      href: "/login/",
+    },
+    {
+      name: "Deposit",
+      href: "/deposit/",
+    },
+    {
+      name: "Withdraw",
+      href: "/withdraw/",
+    },
+    {
+      name: "Balance",
+      href: "/balance/",
+    },
+    {
+      name: "All Data",
+      href: "/alldata",
+    },
+  ];
+
+  useEffect(() => {}, [location]);
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          BadBank
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            {routes.map((route) => {
+              return (
+                <li key={route.name} className="nav-item">
+                  <a
+                    className={
+                      location.pathname.includes(route.href)
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    href={"#" + route.href}
+                  >
+                    {route.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function Spa() {
   return (
     <HashRouter>
